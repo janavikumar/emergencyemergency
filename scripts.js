6d5708bfaef4218a6b076ddf4b6a72455bde4a97
Janavi Kumar <janavikumar@Janavis-Air.lan>
2024-12-06 23:03:00 -0500

    update the text intake and scroll matchups, change design scheme and fix readability

--- scripts.js
+++ scripts.js
@@ -10,7 +10,12 @@ function loadSectionContent(sectionId, filePath) {
 
 // Call this for each section
 loadSectionContent("section-1", "article/ArticlePart1.txt");
+loadSectionContent("section-1_2", "article/article1_2.txt");
+loadSectionContent("section-1_3", "article/article1_3.txt");
+loadSectionContent("section-1_4", "article/article1_4.txt");
 loadSectionContent("section-2", "article/ArticlePart2.txt");
+loadSectionContent("section-2_2", "article/article2_2.txt");
+loadSectionContent("section-2_3", "article/article2_3.txt");
 loadSectionContent("section-3", "article/ArticlePart3.txt");
 
 // Initialize Scrollama
@@ -25,13 +30,23 @@ const visualizations = [
 
 // Function to activate a map
 function activateMap(index) {
-    visualizations.forEach((map, i) => {
-        if (i === index) {
-            map.classList.add("active");
-        } else {
-            map.classList.remove("active");
-        }
-    });
+    if (index <= 3 ) {
+        visualizations[0].classList.add("active"); // Show the first map
+        visualizations[1].classList.remove("active"); // Hide the second map
+        visualizations[2].classList.remove("active"); // Hide the third map
+    }
+    // Show map 2 for the third section
+    else if (index > 3 && index < 7) {
+        visualizations[0].classList.remove("active"); // Hide the first map
+        visualizations[1].classList.add("active"); // Show the second map
+        visualizations[2].classList.remove("active"); // Hide the third map
+    }
+    // Show map 3 for the fourth section
+    else if (index >= 7) {
+        visualizations[0].classList.remove("active"); // Hide the first map
+        visualizations[1].classList.remove("active"); // Hide the second map
+        visualizations[2].classList.add("active"); // Show the third map
+    }
 }
 
 // Set up Scrollama
@@ -42,7 +57,7 @@ scroller
         debug: false, // Enable to visualize trigger points
     })
     .onStepEnter((response) => {
-        const index = parseInt(response.index, 10);
+        let index = parseInt(response.index, 10);
         activateMap(index); // Change the map based on the section
     });
 
